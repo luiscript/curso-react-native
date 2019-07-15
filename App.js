5f5770dc3faf2d734e0b22b84525515bf0ba5eac
luiscript <isc.luisfer@gmail.com>
2019-07-15 18:29:12 -0500

    7.5 Estilos: width & height

--- App.js
+++ App.js
@@ -1,24 +1,12 @@
 import React, { Component } from 'react';
 import {StyleSheet, Text, View } from 'react-native';
 
-const styles = StyleSheet.create({
-  bigBlue: {
-    color: 'blue',
-    fontWeight: 'bold',
-    fontSize: 30,
-  },
-  red: {
-    color: 'red',
-  },
-});
-
 export default function App() {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text style={styles.red}>Rojo</Text>
-      <Text style={styles.bigBlue}>Azul grande</Text>
-      <Text style={[styles.bigBlue, styles.red]}>Azul grande luego rojo</Text>
-      <Text style={[styles.red, styles.bigBlue]}>Rojo luego azul grande</Text>
+      <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+      <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
+      <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
     </View>
   );
 }
