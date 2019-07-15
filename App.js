0e0162412ac1c96e16471f7e3309a687b765eb2a
luiscript <isc.luisfer@gmail.com>
2019-07-15 18:12:32 -0500

    7.3.1 Elementos básicos: Image

--- App.js
+++ App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import Simple from './src/SimpleComponent';
+import { Image, View } from 'react-native';
 
 export default function App() {
+  var pic = {
+    uri: 'https://ouseful.files.wordpress.com/2012/07/proglanginfluence.png'
+  };
   return (
-    <Simple text="Hola React Native!" />
+    <View style={{flex: 1, alignItems: 'center', top: 50}}>
+      <Image source={pic} style={{width: 422, height: 315}}/>
+      <Simple text="Hola React Native!" />
+    </View>
+    
   );
 }
