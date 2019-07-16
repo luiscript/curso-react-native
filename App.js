08fae477049d181429100dacd34e6e0547725162
luiscript <isc.luisfer@gmail.com>
2019-07-15 19:13:50 -0500

    7.5.1 Flex a fondo: flexDirection

--- App.js
+++ App.js
@@ -3,10 +3,10 @@ import {StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{flex: 1 }}>
-      <View style={{flex: 1, backgroundColor: 'powderblue'}} />
-      <View style={{flex: 2, backgroundColor: 'skyblue'}} />
-      <View style={{flex: 3, backgroundColor: 'steelblue'}} />
+    <View style={{flex: 1, flexDirection: 'row'}}>
+      <View style={{width: 100, height: 100, backgroundColor: 'powderblue'}} />
+      <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
+      <View style={{width: 100, height: 100, backgroundColor: 'steelblue'}} />
     </View>
   );
 }
