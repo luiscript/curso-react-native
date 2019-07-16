7514669aa2d4e176b137054ece2223d06c699cbc
luiscript <isc.luisfer@gmail.com>
2019-07-15 19:37:12 -0500

    7.5.2 Flex a fondo: justifyContent

--- App.js
+++ App.js
@@ -3,7 +3,11 @@ import {StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{flex: 1, flexDirection: 'row'}}>
+    <View style={{
+      flex: 1,
+      flexDirection: 'column',
+      justifyContent: 'space-around',
+    }}>
       <View style={{width: 100, height: 100, backgroundColor: 'powderblue'}} />
       <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
       <View style={{width: 100, height: 100, backgroundColor: 'steelblue'}} />
