93ffb2504603bd7262287bafdd6e86b6bbd8c57b
luiscript <isc.luisfer@gmail.com>
2019-07-15 20:02:14 -0500

    7.5.3 Flex a fondo: alignItems

--- App.js
+++ App.js
@@ -6,11 +6,12 @@ export default function App() {
     <View style={{
       flex: 1,
       flexDirection: 'column',
-      justifyContent: 'space-around',
+      justifyContent: 'center',
+      alignItems: 'stretch',
     }}>
-      <View style={{width: 100, height: 100, backgroundColor: 'powderblue'}} />
-      <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
-      <View style={{width: 100, height: 100, backgroundColor: 'steelblue'}} />
+      <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+      <View style={{height: 50, backgroundColor: 'skyblue'}} />
+      <View style={{height: 100, backgroundColor: 'steelblue'}} />
     </View>
   );
 }
