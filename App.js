7f207cad42318021b348b389bb1ebfdbb59ec8ce
luiscript <isc.luisfer@gmail.com>
2019-07-18 16:53:25 -0500

    8.1.2 StackNavigator, navegando entre pantallas

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
 import React from "react";
-import { View, Text } from "react-native";
-import { createStackNavigator, createAppContainer } from "react-navigation";
-
+import { View, Text, Button } from "react-native";
+import { createStackNavigator, createAppContainer  } from "react-navigation";
 class HomeScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         <Text>Home Screen</Text>
+        <Button title="Go to Details"
+          onPress={() => this.props.navigation.navigate('Details')} />
       </View>
     );
   }
 }
-
-const AppNavigator = createStackNavigator({
-  Home: {
-    screen: HomeScreen
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Details Screen</Text>
+      </View>
+    );
   }
-});
-
+}
+const AppNavigator = createStackNavigator(
+  { Home: HomeScreen, Details: DetailsScreen },
+  { initialRouteName: "Home" }
+);
 export default createAppContainer(AppNavigator);
