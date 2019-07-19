f331238e9ffe2de14e4184a86593223cfce07619
luiscript <isc.luisfer@gmail.com>
2019-07-19 08:09:33 -0500

    8.2.2: Personalizando los tabs

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons'; // 6.2.2
 import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
 
 class HomeScreen extends React.Component {
@@ -22,9 +23,68 @@ class SettingsScreen extends React.Component {
   }
 }
 
-const TabNavigator = createBottomTabNavigator({
-  Home: HomeScreen,
-  Settings: SettingsScreen,
-});
+class IconWithBadge extends React.Component {
+  render() {
+    const { name, badgeCount, color, size } = this.props;
+    return (
+      <View style={{ width: 24, height: 24, margin: 5 }}>
+        <Ionicons name={name} size={size} color={color} />
+        {badgeCount > 0 && (
+          <View
+            style={{
+              position: 'absolute',
+              right: -6,
+              top: -3,
+              backgroundColor: 'red',
+              borderRadius: 6,
+              width: 12,
+              height: 12,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Text style={{ color: 'white', fontSize: 10, fontWeight: 'bold' }}>
+              {badgeCount}
+            </Text>
+          </View>
+        )}
+      </View>
+    );
+  }
+}
 
-export default createAppContainer(TabNavigator);
+const HomeIconWithBadge = props => {
+  return <IconWithBadge {...props} badgeCount={3} />;
+};
+
+const getTabBarIcon = (navigation, focused, tintColor) => {
+  const { routeName } = navigation.state;
+  let IconComponent = Ionicons;
+  let iconName;
+  if (routeName === 'Home') {
+    iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+    IconComponent = HomeIconWithBadge;
+  } else if (routeName === 'Settings') {
+    iconName = `ios-options${focused ? '' : '-outline'}`;
+  }
+
+  return <IconComponent name={iconName} size={25} color={tintColor} />;
+};
+
+export default createAppContainer(
+  createBottomTabNavigator(
+    {
+      Home: { screen: HomeScreen },
+      Settings: { screen: SettingsScreen },
+    },
+    {
+      defaultNavigationOptions: ({ navigation }) => ({
+        tabBarIcon: ({ focused, tintColor }) =>
+          getTabBarIcon(navigation, focused, tintColor),
+      }),
+      tabBarOptions: {
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'gray',
+      },
+    }
+  )
+);
